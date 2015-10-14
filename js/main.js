f7dd9097d8d10566ec6b042e87a24a2971ea80af
tm89 <torbjornmevik@gmail.com>
2015-10-14 21:38:39 +0200

    changed function to exclude 1000 in for-loop, gives correct answer (check console)

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ function eulerOne(){
 	var result = 0;
 	var numbers = [];
 	var isMultiple = false;
-	for (var i = 0; i <= 1000; i++) {
-		if (i%3==0) {
-			result+=i;
-			isMultiple=true;
-		}else if (i%5==0) {
-			result+=i;
-			isMultiple=true;
+	for (var i = 0; i < 1000; i++) {
+		if (i % 3 == 0) {
+			result += i;
+			isMultiple = true;
+		} else if (i % 5 == 0) {
+			result += i;
+			isMultiple = true;
 		};
 		if (isMultiple) {
-			numbers.add(i);
-			isMultiple=false;
+			numbers.push(i);
+			isMultiple = false;
 		};
 	}
 	console.log("The result is: " + result);
