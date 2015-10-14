5b3387c274396639d33b8dfae4fe9002e477e7f4
tm89 <torbjornmevik@gmail.com>
2015-10-14 21:16:45 +0200

    Wrote function for first excercise, no test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 function eulerOne(){
-	//multiples of 3 or 5
-	
+	//multiples of 3 or 5 in range [0..1000]
+	var result = 0;
+	var numbers = [];
+	var isMultiple = false;
+	for (var i = 0; i <= 1000; i++) {
+		if (i%3==0) {
+			result+=i;
+			isMultiple=true;
+		}else if (i%5==0) {
+			result+=i;
+			isMultiple=true;
+		};
+		if (isMultiple) {
+			numbers.add(i);
+			isMultiple=false;
+		};
+	}
+	console.log("The result is: " + result);
+	return result;
 }
