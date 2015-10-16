17f72dd1557e2d89a2f9f68498f8ed45d8a0e92e
tm89 <torbjornmevik@gmail.com>
2015-10-16 12:08:26 +0200

    completed excercise 2 with test and time

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function eulerOne(){
 	//multiples of 3 or 5 in range [0..1000]
+	var x = new Date();
 	var result = 0;
 	var numbers = [];
 	var isMultiple = false;
@@ -19,15 +20,18 @@ function eulerOne(){
 	console.log("The result is: " + result);
 	//return result;
 	document.getElementById("answer1").value = result;
+	document.getElementById("time1").value = new Date() - x;
 }
 
 function eulerTwo(){
 	//sum of all even valued fibonacci number with a value that doesn't exceed four mill.
+	var x = new Date();
 	var result = 0;
 	var number = 0;
 	var nrOne = 1;
 	var nrTwo = 1;
-	while(number < 4*10^6){
+	while(number < 4*10e5){
+		console.log('Currently processing: '+ number);
 		number = nrOne + nrTwo;
 		if(number % 2 == 0) {
 			result += number;
@@ -36,5 +40,6 @@ function eulerTwo(){
 		nrTwo = number;
 	}
 	console.log("The result is: " + number);
+	document.getElementById("time2").value = new Date() - x;
 	document.getElementById("answer2").value = result;
 }
