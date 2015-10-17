0291ba9e4651e8a4e3af3575cd38e99f55c063be
tm89 <torbjornmevik@gmail.com>
2015-10-17 10:31:54 +0200

    Added isPrime function to use in excercise 3

--- js/eulersolutionsCtrl.js
+++ js/eulersolutionsCtrl.js
@@ -55,4 +55,18 @@ app.controller('eulersolutionsCtrl',
 			"time": ""
 		}
 		];
+		$scope.executeSolution = function(solution){
+
+		};
+		$scope.isPrime = function(n) {
+			if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false;
+			if (n%2 == 0) return (n==2);
+			if (n%3 == 0) return (n==3);
+			var m = Math.sqrt(n);
+			for (var i=5;i<=m;) {
+				if (n%i==0) return false;
+				if (n%(i+2)==0) return false;
+			}
+			return true;
+		}
 	});
